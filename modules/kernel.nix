1054291fcfd45f3e2b76e743626412149b61b96d
ven-lush-v4 <ven.github@gmail.com>
2026-08-31 16:23:59 +0100

    added a bunch of .nix files, tons of stuff, and boot problems fixed

--- modules/kernel.nix
+++ modules/kernel.nix
@@ -1,42 +1,46 @@
 #kernel.nix
-{ pkgs,...}:
+{ pkgs, ... }:
 {
-# kernel - cachyos(1) or zen(2)
+  # kernel - cachyos(1) or zen(2)
   #boot.kernelPackages = pkgs.cachyosKernels."linuxPackages-cachyos-latest";
   boot.kernelPackages = pkgs.linuxPackages_zen;
 
-  boot.kernelParams = [ "quiet" "splash" "udev.log_priority=3" ];
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "udev.log_priority=3"
+    "usbcore.autosuspend=-1"
+  ];
   boot.kernel.sysctl."vm.swappiness" = 150;
   boot.consoleLogLevel = 3;
   boot.initrd.verbose = false;
   boot.initrd.systemd.enable = false;
   boot.kernel.sysctl = {
-  "vm.vfs_cache_pressure" = 50;  # default 100, keeps file cache longer
-  "vm.dirty_ratio" = 10;
-  "vm.dirty_background_ratio" = 5;
-  "vm.page-cluster" = 0;
-  "vm.dirty_bytes" = 268435456;
+    "vm.vfs_cache_pressure" = 50; # default 100, keeps file cache longer
+    "vm.dirty_ratio" = 10;
+    "vm.dirty_background_ratio" = 5;
+    "vm.page-cluster" = 0;
+    "vm.dirty_bytes" = 268435456;
   };
   powerManagement.cpuFreqGovernor = "schedutil";
   services.udev.extraRules = ''
-  ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/scheduler}="mq-deadline"
+    ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/scheduler}="mq-deadline"
   '';
-systemd.tmpfiles.rules = [
-  "w /sys/kernel/mm/transparent_hugepage/defrag - - - - defer+madvise"
-];
-
+  systemd.tmpfiles.rules = [
+    "w /sys/kernel/mm/transparent_hugepage/defrag - - - - defer+madvise"
+  ];
 
   boot.blacklistedKernelModules = [
-  "joydev"             # joystick - xbox controller on wayland doesn't use this
-  "mousedev"           # legacy mouse interface - wayland uses evdev
-  "mac_hid"            # mac HID compat layer - you're not on a mac lol
-  "serio_raw"          # raw serio interface - nothing needs it
-  "8250_dw"            # designware serial UART - not needed
-  "snd_seq_dummy"      # dummy midi sequencer
-  "dmi_sysfs"          # dmi info via sysfs - not critical
-  "intel_powerclamp"   # intel cpu power clamping - earlyoom handles pressure instead
-  "tiny_power_button"  # alternative power button handler - redundant
-  "ahci"
-  "libahci"
-];
+    "joydev" # joystick - xbox controller on wayland doesn't use this
+    "mousedev" # legacy mouse interface - wayland uses evdev
+    "mac_hid" # mac HID compat layer - you're not on a mac lol
+    "serio_raw" # raw serio interface - nothing needs it
+    "8250_dw" # designware serial UART - not needed
+    "snd_seq_dummy" # dummy midi sequencer
+    "dmi_sysfs" # dmi info via sysfs - not critical
+    "intel_powerclamp" # intel cpu power clamping - earlyoom handles pressure instead
+    "tiny_power_button" # alternative power button handler - redundant
+    "ahci"
+    "libahci"
+  ];
 }
